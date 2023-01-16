37b157ad02312ccb3fd9eaffdfb5a15ab7094e0d
Evgeniya <74735099+sanny410@users.noreply.github.com>
2023-01-16 13:02:28 +0300

    Исправила импорты

--- src/Core/AppRouter.tsx
+++ src/Core/AppRouter.tsx
@@ -1,9 +1,9 @@
 import {FunctionComponent} from 'react';
 
 import ROUTES from 'Core/Const/Routes';
-import HelloWorldPage from 'Pages/HelloWorldPage';
-import NotFoundPage from 'Pages/NotFoundPage';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import HelloWorldPage from 'UI/Pages/HelloWorldPage';
+import NotFoundPage from 'UI/Pages/NotFoundPage';
 
 /**
  * Роутер приложения
---
+++ src/Core/Const/DateTimeFormat.ts
@@ -0,0 +1,8 @@
+/** Формат дня */
+export const DayFormat = 'DD';
+/** Формат месяца */
+export const MonthFormat = 'MM';
+/** Формат года */
+export const YearFormat = 'YY';
+
+export const DateFormat = `${DayFormat}.${MonthFormat}.${YearFormat}`;
