ebbbc645025750e21117346347c4a66d8858705f
Evgeniya <74735099+sanny410@users.noreply.github.com>
2023-04-19 17:56:01 +0300

    Вынесено отдельно isTaskTitleDuplicated

--- src/Services/Gateways/Task/index.ts
+++ src/Services/Gateways/Task/index.ts
@@ -1,6 +1,6 @@
 import {TaskDuplicationError} from 'Application/exceptions/tasks';
 import {DATE_FORMAT} from 'Core/Const/DateTimeFormat';
-import {Task, TaskCreatingSummary, TaskUpdateSummary, TaskId, updateTaskList} from 'Domain/task';
+import {Task, TaskCreatingSummary, TaskUpdateSummary, TaskId, updateTaskList, TaskTitle} from 'Domain/task';
 import moment from 'moment';
 import {v4 as uuidv4} from 'uuid';
 
@@ -23,18 +23,20 @@ const taskGateway = {
 
         const tasks: Task[] = JSON.parse(localStorage.getItem('tasks') ?? '[]');
 
-        const isTaskTitleDuplicated = tasks.some((_task) => _task.title === summary.title);
+        if (this.isTaskTitleDuplicated(tasks, task.title)) throw new TaskDuplicationError();
 
-        if (isTaskTitleDuplicated) {
-            throw new TaskDuplicationError();
-        } else tasks.push(task);
+        tasks.push(task);
         localStorage.setItem('tasks', JSON.stringify(tasks));
 
         return task;
     },
     async update(summary: TaskUpdateSummary, id: TaskId): Promise<Task> {
         const tasks: Task[] = JSON.parse(localStorage.getItem('tasks') ?? '[]');
+
         let updateTask: Task = tasks.find((item) => item.id === id) as Task;
+
+        if (updateTask.title !== summary.title && this.isTaskTitleDuplicated(tasks, updateTask.title))
+            throw new TaskDuplicationError();
 
         updateTask = {
             id: updateTask.id,
@@ -63,6 +65,9 @@ const taskGateway = {
         localStorage.setItem('tasks', JSON.stringify(taskList));
 
         return deleteTask;
+    },
+    isTaskTitleDuplicated(tasks: Task[], title: TaskTitle): boolean {
+        return tasks.some((_task) => _task.title === title);
     },
 };
 export default taskGateway;
